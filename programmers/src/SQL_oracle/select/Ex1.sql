3709125c0f458a88510bffe1382b6212d0f1d95c
Youngmin Moon <83321379+Moon-ymin@users.noreply.github.com>
2024-03-13 10:29:42 +0900

    to_char() 함수

--- programmers/src/SQL_oracle/select/Ex1.sql
+++ programmers/src/SQL_oracle/select/Ex1.sql
@@ -1,4 +1,6 @@
-/* https://school.programmers.co.kr/learn/courses/30/lessons/131120 */
+/* 3월에 태어난 여성 회원 목록 출력하기
+- https://school.programmers.co.kr/learn/courses/30/lessons/131120 
+*/
 
 SELECT member_id, member_name, gender, to_char(date_of_birth,'YYYY-MM-DD') as date_of_birth
 from member_profile
