4563374216a606f21253147206c2bdb91971e8bb
Youngmin Moon <83321379+Moon-ymin@users.noreply.github.com>
2024-03-13 10:20:59 +0900

    to_char() 함수

---
+++ programmers/src/SQL_oracle/select/Ex1.sql
@@ -0,0 +1,8 @@
+/* https://school.programmers.co.kr/learn/courses/30/lessons/131120 */
+
+SELECT member_id, member_name, gender, to_char(date_of_birth,'YYYY-MM-DD') as date_of_birth
+from member_profile
+where to_char(date_of_birth, 'MM') = '03'  
+and gender = 'W'
+and tlno is not NULL
+order by member_id;
