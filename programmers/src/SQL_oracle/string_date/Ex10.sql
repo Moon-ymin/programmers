59473be3fb75610c06cc63edef18beeb42f76f29
Youngmin Moon <83321379+Moon-ymin@users.noreply.github.com>
2024-03-27 10:34:06 +0900

    Update Ex10.sql

--- programmers/src/SQL_oracle/string_date/Ex10.sql
+++ programmers/src/SQL_oracle/string_date/Ex10.sql
@@ -3,9 +3,9 @@
 */
 select b.apnt_no, b.pt_name, b.pt_no, mcdp_cd, d.dr_name, b.apnt_ymd
 from doctor d join
-    ( SELECT a.mddr_id, a.apnt_no, a.apnt_ymd, a.apnt_cncl_yn, a.pt_no, p.pt_name
+    ( SELECT a.mddr_id, a.apnt_no, a.apnt_ymd, a.pt_no, p.pt_name
     from appointment a join patient p on a.pt_no = p.pt_no
-    where a.mcdp_cd = 'CS' ) b 
+    where a.mcdp_cd = 'CS'
+    and to_char(a.apnt_ymd,'yyyy-mm-dd') like '2022-04-13' and a.apnt_cncl_yn = 'N') b 
 on d.dr_id = b.mddr_id
-where to_char(b.apnt_ymd,'yyyy-mm-dd') like '2022-04-%' and b.apnt_cncl_yn = 'N'
-order by apnt_ymd asc;
+order by 6 asc;
