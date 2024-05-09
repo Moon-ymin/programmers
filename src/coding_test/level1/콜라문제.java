fc2db71a70dea5caa5184f3db1dcb39ef47be935
moonyoungmin <ansdudals8@naver.com>
2024-05-09 23:55:37 +0900

    create

---
+++ src/coding_test/level1/콜라문제.java
@@ -0,0 +1,15 @@
+package coding_test.level1;
+
+// 콜라문제 : https://school.programmers.co.kr/learn/courses/30/lessons/132267
+class Solution {
+    public int solution(int a, int b, int n) {
+        int answer = 0;
+
+        while(n >= a) {
+            answer += (n / a) * b;
+            n = (n/a) * b + (n%a);
+            System.out.println(answer);
+        }
+        return answer;
+    }
+}
